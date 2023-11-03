be85091d1d913924c5171154f30372b2c12490cd
Colum Peters <g00427223@atu.ie>
2023-11-03 10:22:26 +0000

    Completed all 3 questions for assignment. Using 3 different constructors which accept different parameters each time

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,17 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
+        Person student1 = new Person();
+        student1.getUserInput();
 
+        Person student2 = new Person("");
+        student2.getUserInput();
+
+        Person student3 = new Person("", "", "");
+        student3.getUserInput();
+
+        student1.displayUser();
+        student2.displayUser();
+        student3.displayUser();
     }
 }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,81 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class Person {
+    private String userName;
+    private String userEmail;
+    private String userCourse;
+
+    //Constructors
+    //Q1. Using getter and setter methods for all parameters
+    public Person(){
+        this.userName = "";
+        this.userEmail = "";
+        this.userCourse = "";
+    }
+
+    //Q2. Using getter and setter methods for Email & Course
+    //Q2. Constructor accepts userName
+    public Person(String userName){
+        this.userName = userName;
+        this.userEmail = "";
+        this.userCourse = "";
+    }
+
+    //Q3. Constructor accepts Name, Email and Course parameters
+    public Person(String userName, String userEmail, String userCourse) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userCourse = userCourse;
+    }
+
+
+    //Method to get user input
+    public void getUserInput(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your full name: ");
+        this.userName = scanner.nextLine();
+
+        System.out.println("Please enter your email: ");
+        this.userEmail = scanner.nextLine();
+
+        System.out.println("Please enter your course: ");
+        this.userCourse = scanner.nextLine();
+    }
+
+    //Method to display user details
+    public void displayUser(){
+        System.out.println("-----------------");
+        System.out.println(getUserName());
+        System.out.println(getUserEmail());
+        System.out.println(getUserCourse());
+        System.out.println("-----------------");
+        System.out.println("\n");
+    }
+
+    //Getter & Setter Methods / Accessor Methods
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserCourse() {
+        return userCourse;
+    }
+
+    public void setUserCourse(String userCourse) {
+        this.userCourse = userCourse;
+    }
 }
